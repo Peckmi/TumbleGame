fa5abcbff7f85bcacc1c03ba0f93fdf103248e69
Ethan <white2e9@mail.uc.edu>
2024-04-01 13:31:26 -0400

    configured game and added a temporary background image

--- game.js
+++ game.js
@@ -1,1 +1,30 @@
-var game = new Phaser.Game();
+var config = {
+    type: Phaser.AUTO,
+    width: 800,
+    height: 600,
+    physics: {
+        default: 'arcade',
+        arcade: {
+            debug: false
+        }
+    },
+    scene: {
+        preload: preload,
+        create: create,
+        update: update
+    }
+}
+
+var game = new Phaser.Game(config);
+
+function preload() {
+    this.load.image('background', './assets/tempBack.png');
+}
+
+function create() {
+    this.add.image(400, 300, 'background');
+}
+
+function update() {
+
+}
